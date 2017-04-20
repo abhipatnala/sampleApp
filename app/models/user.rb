2cda01c5859ac874d21d32d401130496228d99a6
avinash patnala <avinashpatnala@avinashs-MacBook-Air.local>
2017-04-20 21:46:21 +0530

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 	 attr_accessor :remember_token
 	 before_save { self.email = email.downcase }
 	validates :name, presence: true,length: { maximum: 50 }
@@ -12,6 +15,9 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  def downcase_email
+      self.email = email.downcase
+    end
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -19,11 +25,25 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+ def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   def forget
     update_attribute(:remember_digest, nil)
+  end
+  def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    
+    def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
    def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-     password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
   end
 
   test "should be valid" do
@@ -48,7 +51,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
-  end
+  
 end
