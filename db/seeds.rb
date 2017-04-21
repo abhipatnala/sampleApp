481e293fb30fceb1380b1c84adb69237d2264c80
avinash patnala <avinashpatnala@avinashs-MacBook-Air.local>
2017-04-21 11:44:18 +0530

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -11,13 +12,14 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
